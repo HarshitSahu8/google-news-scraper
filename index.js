e74868dd05db36d6e8f71b2eecc33c3cc86e12a3
harshitsahu8 <harshitsahu8564@gmail.com>
2024-09-22 12:25:48 +0530

    adding path of chrome exec

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-'use strict'
+"use strict";
 
-const puppeteer = require('puppeteer');
-const cheerio = require('cheerio');
+const puppeteer = require("puppeteer");
+const cheerio = require("cheerio");
 
-const getLogger = require('./logger');
-const getTitle = require('./getTitle').default;
-const getArticleType = require('./getArticleType').default;
-const getPrettyUrl = require('./getPrettyUrl').default;
-const buildQueryString = require('./buildQueryString').default;
-const getArticleContent = require('./getArticleContent').default;
+const getLogger = require("./logger");
+const getTitle = require("./getTitle").default;
+const getArticleType = require("./getArticleType").default;
+const getPrettyUrl = require("./getPrettyUrl").default;
+const buildQueryString = require("./buildQueryString").default;
+const getArticleContent = require("./getArticleContent").default;
 
 const googleNewsScraper = async (userConfig) => {
-
-  const config = Object.assign({
-    prettyURLs: true,
-    getArticleContent: false,
-    puppeteerArgs: [],
-    puppeteerHeadlessMode: true, 
-    logLevel: 'error',
-  }, userConfig);
+  const config = Object.assign(
+    {
+      prettyURLs: true,
+      getArticleContent: false,
+      puppeteerArgs: [],
+      puppeteerHeadlessMode: true,
+      chromePath: null, // Option for custom Chrome path
+      logLevel: "error",
+    },
+    userConfig
+  );
 
   const logger = getLogger(config.logLevel);
 
@@ -27,89 +30,124 @@ const googleNewsScraper = async (userConfig) => {
     queryVars.q = userConfig.searchTerm;
   }
 
-  const queryString = queryVars ? buildQueryString(queryVars) : ''
-  const baseUrl = config.baseUrl ?? `https://news.google.com/search`
-  const timeString = config.timeframe ? ` when:${config.timeframe}` : ''
-  const url = `${baseUrl}${queryString}${timeString}`
+  const queryString = queryVars ? buildQueryString(queryVars) : "";
+  const baseUrl = config.baseUrl ?? `https://news.google.com/search`;
+  const timeString = config.timeframe ? ` when:${config.timeframe}` : "";
+  const url = `${baseUrl}${queryString}${timeString}`;
 
   logger.info(`📰 SCRAPING NEWS FROM: ${url}`);
+
   const requiredArgs = [
-    '--disable-extensions-except=/path/to/manifest/folder/',
-    '--load-extension=/path/to/manifest/folder/',
+    "--disable-extensions-except=/path/to/manifest/folder/",
+    "--load-extension=/path/to/manifest/folder/",
   ];
+
+  // Set up Puppeteer configuration, checking for custom Chrome path
   const puppeteerConfig = {
     headless: userConfig.puppeteerHeadlessMode,
-    args: puppeteer.defaultArgs().concat(config.puppeteerArgs).filter(Boolean).concat(requiredArgs)
-  }
-  const browser = await puppeteer.launch(puppeteerConfig)
-  const page = await browser.newPage()
-  page.setViewport({ width: 1366, height: 768 })
-  page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36')
-  page.setRequestInterception(true)
-  page.on('request', request => {
+    args: puppeteer
+      .defaultArgs()
+      .concat(config.puppeteerArgs)
+      .filter(Boolean)
+      .concat(requiredArgs),
+    executablePath: config.chromePath || undefined, // Use user-provided path or default
+  };
+
+  const browser = await puppeteer.launch(puppeteerConfig);
+  const page = await browser.newPage();
+  page.setViewport({ width: 1366, height: 768 });
+  page.setUserAgent(
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
+  );
+
+  page.setRequestInterception(true);
+  page.on("request", (request) => {
     if (!request.isNavigationRequest()) {
-      request.continue()
-      return
+      request.continue();
+      return;
     }
-    const headers = request.headers()
-    headers['Accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3'
-    headers['Accept-Encoding'] = 'gzip'
-    headers['Accept-Language'] = 'en-US,en;q=0.9,es;q=0.8'
-    headers['Upgrade-Insecure-Requests'] = "1"
-    headers['Referer'] = 'https://www.google.com/'
-    request.continue({ headers })
-  })
+    const headers = request.headers();
+    headers["Accept"] =
+      "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3";
+    headers["Accept-Encoding"] = "gzip";
+    headers["Accept-Language"] = "en-US,en;q=0.9,es;q=0.8";
+    headers["Upgrade-Insecure-Requests"] = "1";
+    headers["Referer"] = "https://www.google.com/";
+    request.continue({ headers });
+  });
+
   await page.setCookie({
     name: "CONSENT",
-    value: `YES+cb.${new Date().toISOString().split('T')[0].replace(/-/g, '')}-04-p0.en-GB+FX+667`,
-    domain: ".google.com"
+    value: `YES+cb.${new Date()
+      .toISOString()
+      .split("T")[0]
+      .replace(/-/g, "")}-04-p0.en-GB+FX+667`,
+    domain: ".google.com",
   });
-  await page.goto(url, { waitUntil: 'networkidle2' });
+
+  await page.goto(url, { waitUntil: "networkidle2" });
 
   try {
     await page.$(`[aria-label="Reject all"]`);
     await Promise.all([
       page.click(`[aria-label="Reject all"]`),
-      page.waitForNavigation({ waitUntil: 'networkidle2' })
+      page.waitForNavigation({ waitUntil: "networkidle2" }),
     ]);
   } catch (err) {}
 
   const content = await page.content();
   const $ = cheerio.load(content);
 
-  const articles = $('article');
-  let results = []
-  let i = 0
-  const urlChecklist = []
+  const articles = $("article");
+  let results = [];
+  let i = 0;
+  const urlChecklist = [];
 
   $(articles).each(function () {
-    const link = $(this)?.find('a[href^="./article"]')?.attr('href')?.replace('./', 'https://news.google.com/') || $(this)?.find('a[href^="./read"]')?.attr('href')?.replace('./', 'https://news.google.com/') || false
+    const link =
+      $(this)
+        ?.find('a[href^="./article"]')
+        ?.attr("href")
+        ?.replace("./", "https://news.google.com/") ||
+      $(this)
+        ?.find('a[href^="./read"]')
+        ?.attr("href")
+        ?.replace("./", "https://news.google.com/") ||
+      false;
     link && urlChecklist.push(link);
-    const srcset = $(this).find('figure').find('img').attr('srcset')?.split(' ');
-    const image = srcset && srcset.length
-      ? srcset[srcset.length - 2]
-      : $(this).find('figure').find('img').attr('src');
+    const srcset = $(this)
+      .find("figure")
+      .find("img")
+      .attr("srcset")
+      ?.split(" ");
+    const image =
+      srcset && srcset.length
+        ? srcset[srcset.length - 2]
+        : $(this).find("figure").find("img").attr("src");
     const articleType = getArticleType($, this);
     const title = getTitle($, this, articleType);
     const mainArticle = {
-      title, 
-      "link": link,
-      "image": image?.startsWith("/") ? `https://news.google.com${image}` : image,
-      "source": $(this).find('div[data-n-tid]').text() || false,
-      "datetime": new Date($(this).find('div:last-child time').attr('datetime')) || false,
-      "time": $(this).find('div:last-child time').text() || false, 
-      articleType
-    }
-    results.push(mainArticle)
-    i++
+      title,
+      link: link,
+      image: image?.startsWith("/") ? `https://news.google.com${image}` : image,
+      source: $(this).find("div[data-n-tid]").text() || false,
+      datetime:
+        new Date($(this).find("div:last-child time").attr("datetime")) || false,
+      time: $(this).find("div:last-child time").text() || false,
+      articleType,
+    };
+    results.push(mainArticle);
+    i++;
   });
 
   if (config.prettyURLs) {
-    results = await Promise.all(results.map(article => {
-      const url = getPrettyUrl(article.link, logger);
-      article.link = url;
-      return article;
-    }));
+    results = await Promise.all(
+      results.map((article) => {
+        const url = getPrettyUrl(article.link, logger);
+        article.link = url;
+        return article;
+      })
+    );
   }
 
   if (config.getArticleContent) {
@@ -118,10 +156,9 @@ const googleNewsScraper = async (userConfig) => {
   }
 
   await page.close();
-  await browser.close()
+  await browser.close();
 
-  return results.filter(result => result.title)
-
-}
+  return results.filter((result) => result.title);
+};
 
 module.exports = googleNewsScraper;
